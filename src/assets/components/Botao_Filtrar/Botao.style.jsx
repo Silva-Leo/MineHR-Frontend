f045a2f19f47ca784928582c4c047bf6e885a071
Leo|Xfce <devleonardosilva@gmail.com>
2022-06-11 20:39:05 -0300

    FEAT: Componente botao criado

---
+++ src/assets/components/Botao_Filtrar/Botao.style.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from 'styled-components';
+
+export const Botao = styled.button`
+  font-size: 15px;
+  background-color: #5d405c;
+  color: #fff;
+  border-radius: 10px;
+  border: none;
+  display: flex;
+  justify-content: space-around;
+  width: 7%;
+  height: 60%;
+  align-items: center;
+`;
