f8d6f5f6d94f551cdaad3e19c9db0f16eea6e768
Leo|Xfce <devleonardosilva@gmail.com>
2022-06-11 20:48:27 -0300

    FEAT: Section de titulo e texto completa

--- src/assets/components/Dashboard/Dashboard.jsx
+++ src/assets/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { StyleDashboard } from "./Dashboard.style";
 import { Section } from "./Dashboard.style";
+import { Div_Texto } from "./Dashboard.style";
+import { Titulo } from "./Dashboard.style";
+import { SubTitulo } from "./Dashboard.style";
+import { BotaoFiltrar } from "../Botao_Filtrar/Botao_Filtrar";
 
 export const Dashboard = () => {
     return (
         <StyleDashboard>
-    
+            <Section>
+                <Div_Texto>
+                    <Titulo>Dashboard</Titulo>
+                    <SubTitulo>Desafio Técnico Frontend</SubTitulo>
+                </Div_Texto>
+                <BotaoFiltrar />
+            </Section>
         </StyleDashboard>
     );
 };
--- src/assets/components/Dashboard/Dashboard.style.jsx
+++ src/assets/components/Dashboard/Dashboard.style.jsx
@@ -18,3 +18,20 @@ export const Section = styled.div`
   align-items: baseline;
 `;
 
+export const Titulo = styled.h1`
+  color: #5d405c;
+`;
+
+export const SubTitulo = styled.span`
+  font-size: 15px;
+  font-weight: 200;
+  letter-spacing: 1px;
+`;
+
+export const Div_Texto = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
