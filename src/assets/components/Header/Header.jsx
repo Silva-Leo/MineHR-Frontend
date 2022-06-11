77738a5e119bff60118de4c7c6dbebd2eff63005
Leo|Xfce <devleonardosilva@gmail.com>
2022-06-11 20:37:43 -0300

    FEAT: Cabeçalho feito

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Header() {
-  return (
-    <>
-    
-    </>
-  );
-}
+export const Header=styled.div`
+    display:flex ;
+    width: 100%;
+    background-color: #CEC6CE;
+    height: 6vw; /* 6 v */
+`
