828dd6e9c5a8e99db9df8d8810dc1408ad4b8e5e
Leo|Xfce <devleonardosilva@gmail.com>
2022-06-11 20:42:48 -0300

    STYLE: Estilo dashboard definido

---
+++ src/assets/components/Dashboard/Dashboard.style.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export const StyleDashboard = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  padding: 3% 3%;
+  flex-direction:column;
+  `
+
