efa6f35814a8eb431cb45b8b84b20a96a1025a81
Leo|Xfce <devleonardosilva@gmail.com>
2022-06-11 20:40:04 -0300

    FEAT: Componente Layout criado

---
+++ src/assets/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "../Header/Header";
+import { Dashboard } from "../Dashboard/Dashboard";
+
+function Layout() {
+    return(
+        <>
+            <Header />
+            <Dashboard/>
+        </>
+  )
+}
+
+export default Layout;
