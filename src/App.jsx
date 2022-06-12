533ab3c9a889b19811b624b93dc174e643121b49
Leo|Xfce <devleonardosilva@gmail.com>
2022-06-12 20:43:07 -0300

    Deploy to my site is ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import GlobalStyle from "./style/Global.style.jsx";
 import Layout from "./components/Layout/Layout.jsx";
 
-// import './App.css'
 
 function App() {
     return (
